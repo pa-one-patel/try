614b18066e1e41560f74b12e1be1ee0e814ce3f9
Pavan patel <40932927+pa-one-patel@users.noreply.github.com>
2022-07-04 16:20:03 +0530

    Update output.sh

--- output.sh
+++ output.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-
+var RUNDECK_VERSION = 55;
 echo "here we are creating some file , this file will be saved in output directory as 'some-file' , and will be used as inpute for other tast " > some-file/file1
 echo "file2" > some-file/file2
+echo "{\"RUNDECK_VERSION\":\"hi pavan\"}" > some-file/build_args.json
+echo "{\"RUNDECK_VERSION\":\"${RUNDECK_VERSION}\"}" > some-file/build_args.json
+
 
 
 #ls some-file/*
